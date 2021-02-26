216a588e03f175460d701d59bd00baa59bf83927
Joel Godfrey-Smith <jgodfrey-smith@pm.me>
2021-02-25 21:54:30 -0500

    20210225

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import json
+from difflib import get_close_matches
 
 data = json.load(open("data.json"))
 
@@ -7,6 +8,16 @@ def definition(w):
     w = w.lower()
     if w in data:
         return data[w]
+    elif get_close_matches(w, data.keys(), cutoff=0.8):
+        ans = input(
+            "Did you mean: %s? Enter Y for yes or N for no: " % get_close_matches(w, data.keys(), cutoff=0.8)[0])
+        if ans == "Y" or "y":
+            # TODO: figure out why this return line breaks the program
+            return data[get_close_matches(w, data.keys(), cutoff=0.8)[0]]
+        elif ans == "N" or "n":
+            return "Try again"
+        else:
+            return "Error"
     else:
         return "Word not found."
 
